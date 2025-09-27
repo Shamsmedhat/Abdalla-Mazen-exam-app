c4ebd20f0d457df0bc237590e6c7c26a914b79f7
abdalla-mazen <armn404040@gmail.com>
2025-09-27 22:19:02 +0200

    second change

--- src/app/dashboard/@content/setting/layout.tsx
+++ src/app/dashboard/@content/setting/layout.tsx
@@ -1,52 +1,5 @@
-// import { ChevronLeft, UserRound } from "lucide-react";
-// import { DynamicBreadcrumb } from "../../_components/bread-crumb";
-// import SideBarSetting from "./@settingside/page";
-
 import { ChevronLeft, UserRound } from "lucide-react";
 import { DynamicBreadcrumb } from "../../_components/bread-crumb";
-
-// export default function SettingLayout({
- 
-//   settings,
-// }: {
-//   children: React.ReactNode;
-//   sideBarSetting: React.ReactNode;
-//   settings: React.ReactNode;
-// }) {
-//   return (
-//     <div className="flex flex-col h-screen">
-//       {/* Header */}
-//       <header className="bg-white">
-//         <div className="p-4">
-//           <DynamicBreadcrumb />
-//         </div>
-//         <div className="flex justify-between gap-2 px-6 py-4 bg-gray-100">
-//           <div className="border border-blue-600 flex items-center">
-//             <ChevronLeft className="mx-2 text-blue-600" />
-//           </div>
-//           <div className="flex bg-blue-600 p-4 gap-4 text-white w-full">
-//             <UserRound size={45} />
-//             <h1 className="text-3xl font-semibold">Account Settings</h1>
-//           </div>
-//         </div>
-//       </header>
-
-//       {/* Content */}
-//       <div className="flex flex-1 h-[calc(100vh-52px)]">
-//         {/* Sidebar */}
-//         <aside className="max-w-96 w-72 mx-6 p-10 bg-white overflow-auto">
-//           <SideBarSetting />
-//         </aside>
-
-//         {/* Main content */}
-//         <main className="flex-1 bg-gray-100 overflow-auto">
-//           {settings}
-//         </main>
-//       </div>
-//     </div>
-//   );
-// }
-
 
 export default function SettingLayout({
   children,
@@ -91,4 +44,3 @@ export default function SettingLayout({
     </div>
   );
 }
-
